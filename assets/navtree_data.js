a52234b4164500bcf413741b7c638ea1ea73347c
MuleSoft <code-contrib@mulesoft.com>
2012-02-24 05:01:36 -0800

    Updating documentation

---
+++ assets/navtree_data.js
@@ -0,0 +1,13 @@
+var NAVTREE_DATA =
+[ [ "org.mule.module.maven", "org/mule/module/maven/package-summary.html", [ [ "Classes", null, [ [ "MavenConnector", "org/mule/module/maven/MavenConnector.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.maven.adapters", "org/mule/module/maven/adapters/package-summary.html", [ [ "Classes", null, [ [ "MavenConnectorCapabilitiesAdapter", "org/mule/module/maven/adapters/MavenConnectorCapabilitiesAdapter.html", null, "" ], [ "MavenConnectorLifecycleAdapter", "org/mule/module/maven/adapters/MavenConnectorLifecycleAdapter.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.maven.config", "org/mule/module/maven/config/package-summary.html", [ [ "Classes", null, [ [ "ExecuteGoalDefinitionParser", "org/mule/module/maven/config/ExecuteGoalDefinitionParser.html", null, "" ], [ "MavenConnectorConfigDefinitionParser", "org/mule/module/maven/config/MavenConnectorConfigDefinitionParser.html", null, "" ], [ "MavenConnectorNamespaceHandler", "org/mule/module/maven/config/MavenConnectorNamespaceHandler.html", null, "" ] ]
+, "" ] ]
+, "" ], [ "org.mule.module.maven.processors", "org/mule/module/maven/processors/package-summary.html", [ [ "Classes", null, [ [ "ExecuteGoalMessageProcessor", "org/mule/module/maven/processors/ExecuteGoalMessageProcessor.html", null, "" ] ]
+, "" ] ]
+, "" ] ]
+
+;
+
---
+++ java/lists.js
@@ -0,0 +1,15 @@
+var DATA = [
+      { id:0, label:"execute-goal", link:"mule/maven.html#execute-goal", type:"method" },
+      { id:1, label:"org.mule.module.maven", link:"java/org/mule/module/maven/package-summary.html", type:"package" },
+      { id:2, label:"org.mule.module.maven.MavenConnector", link:"java/org/mule/module/maven/MavenConnector.html", type:"class" },
+      { id:3, label:"org.mule.module.maven.adapters", link:"java/org/mule/module/maven/adapters/package-summary.html", type:"package" },
+      { id:4, label:"org.mule.module.maven.adapters.MavenConnectorCapabilitiesAdapter", link:"java/org/mule/module/maven/adapters/MavenConnectorCapabilitiesAdapter.html", type:"class" },
+      { id:5, label:"org.mule.module.maven.adapters.MavenConnectorLifecycleAdapter", link:"java/org/mule/module/maven/adapters/MavenConnectorLifecycleAdapter.html", type:"class" },
+      { id:6, label:"org.mule.module.maven.config", link:"java/org/mule/module/maven/config/package-summary.html", type:"package" },
+      { id:7, label:"org.mule.module.maven.config.ExecuteGoalDefinitionParser", link:"java/org/mule/module/maven/config/ExecuteGoalDefinitionParser.html", type:"class" },
+      { id:8, label:"org.mule.module.maven.config.MavenConnectorConfigDefinitionParser", link:"java/org/mule/module/maven/config/MavenConnectorConfigDefinitionParser.html", type:"class" },
+      { id:9, label:"org.mule.module.maven.config.MavenConnectorNamespaceHandler", link:"java/org/mule/module/maven/config/MavenConnectorNamespaceHandler.html", type:"class" },
+      { id:10, label:"org.mule.module.maven.processors", link:"java/org/mule/module/maven/processors/package-summary.html", type:"package" },
+      { id:11, label:"org.mule.module.maven.processors.ExecuteGoalMessageProcessor", link:"java/org/mule/module/maven/processors/ExecuteGoalMessageProcessor.html", type:"class" }
+
+    ];
